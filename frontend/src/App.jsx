f9c0cd27e2a8f0b894fae660139a853d33cd69d3
Alexander Jung <AJung@o365.comhard.de>
2025-06-01 23:00:46 +0000

    Add MyRecipes and Favorites routes to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import MyRecipes from './pages/MyRecipes';
 import LoginPage from './pages/LoginPage';
 import RecipeDetail from './pages/RecipeDetail';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/me" element={<MyRecipes />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/recipes/:id" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
