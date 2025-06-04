03d4459eba11a42b5660f8a52b1ca68a2a747d90
Alexander Jung <AJung@o365.comhard.de>
2025-06-04 22:52:14 +0000

    Configure Vite server to use HTTPS with SSL certificate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: 'ajubuntu',
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../backend/ssl/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../backend/ssl/cert.pem')),
+    },
     allowedHosts: ['ajubuntu']
-  },
-})
+  }
+});
